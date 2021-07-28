36ff74b952e292f1cecc14073a74d1ebbb2eec1c
Daniel <damejias23@gmail.com>
2021-07-28 16:43:05 +0200

    Test

--- Nodo_Buffer/main.py
+++ Nodo_Buffer/main.py
@@ -13,11 +13,11 @@ import _thread
 #lock = _thread.allocate_lock()
 
 # A basic package header, B: 1 byte for the deviceId, B: 1 byte for the pkg size, %ds: Formatted string for string
-_LORA_PKG_FORMAT = "BBBBI"
+_LORA_PKG_FORMAT = "!BBIBB"
 
 
 # A basic ack package, B: 1 byte for the deviceId, B: 1 byte for the pkg size, B: 1 byte for the Ok (200) or error messages
-_LORA_PKG_ACK_FORMAT = "BBB"
+_LORA_PKG_ACK_FORMAT = "!BBB"
 
 MAX_RECV = 1
 
@@ -46,7 +46,9 @@ dist = 10
 list = []
 list_recv = []
 count_list = []
-buffer_msg = []
+
+buffer = []
+
 
 #lista ID para enviar y bloquear
 if(modeTEST):
@@ -59,36 +61,40 @@ def Send():
     global lora, lora_sock, Px_Rx, location, ID_send, id_device, list_recv
 
     while(True):
-        if(len(buffer_msg) > 0):
-            send = buffer_msg.pop(0)
-            
+        if(ID_send and len(buffer)>0):
+            #if id_send not in list:
+        #ENVIO
+            send_pck = buffer.pop(0)
+            id_to_send, id_end, UID_msg = get_IDdest(recv_pkg)
+            if(id_to_send == id_device):
+                lora_sock.send(send_pck)
+            else:
+                pck_reenvio = set_IDorigen(send_pck, id_device)
+                lora_sock.send(pck_reenvio)
 
 
+def Packet_buffer():
+    global Px_Rx, location, ID_send, id_device, list_recv, buffer
 
-
+    while(True):
         if(ID_send):
             #if id_send not in list:
         #ENVIO
             UID = UID_message(id_device)
             list_recv.append(UID)
-            pck = pack_Lora(id_device, location, Px_Rx, ID_send, UID)
-            lora_sock.send(pck)
+            pck = pack_Lora(id_device, ID_send, UID, location, Px_Rx)
+            buffer.append(pck)
             time.sleep(15)
 
-
-
-def Resp(ID_respond):
-
-
 def Recv():
-    global lora, lora_sock, Px_Rx, id_recv, id_send, location, id_device, dist, list_recv, modeTEST
+    global lora, lora_sock, Px_Rx, id_recv, id_send, location, id_device, dist, list_recv, modeTEST, buffer
     while(True):
         #Recibo
         recv_pkg = lora_sock.recv(256)
         #if(len(recv_pkg) != 0): print("Tamaño = %d" % len(recv_pkg))
         if (len(recv_pkg) > len(_LORA_PKG_FORMAT) -1):
             recv_pkg_len = recv_pkg[1]
-            id_to_send, location, My_px, id_end, UID_msg = unpack_Lora(recv_pkg,  recv_pkg_len)
+            id_to_send, id_end, UID_msg = get_IDdest(recv_pkg)
             if(modeTEST):
                 if id_to_send in ID_block:
                     continue
@@ -97,15 +103,18 @@ def Recv():
             else:
                 list_recv.append(UID_msg)
             if(id_end == id_device):
+                id_to_send, id_end, UID_msg, location, My_px  = unpack_Lora(recv_pkg,  recv_pkg_len)
                 print("Mensaje para mi de %d" % get_SendID(UID_msg))
-            """elif(id_end == 1):
+            """elif(id_end == 555):
+                id_to_send, id_end, UID_msg, location, My_px  = unpack_Lora(recv_pkg,  recv_pkg_len)
                 print("Mensaje broadcast: %d" % get_SendID(UID_msg))
-                lora_sock.send(pack_Lora(id_device, location, My_px, id_end, UID_msg))"""
-                #lora_sock.send(recv_pkg)
+                pck_reenvio = set_IDorigen(recv_pkg, id_device)
+                lora_sock.send(pck_reenvio)"""
             else:
-                #print("Reenvio de: %d" % id_to_send)
-                buffer_msg.append(recv_pkg)
-                #lora_sock.send(recv_pkg)
+                """print("Reenvio de: %d" % id_to_send)
+                pck_reenvio = set_IDorigen(recv_pkg, id_device)
+                lora_sock.send(pck_reenvio)"""
+                buffer.append(recv_pkg)
 
 
 def clear():
@@ -113,7 +122,7 @@ def clear():
     time.sleep(200)
     list_recv = []
 
-"""
+
 def lora_cb(lora):
     events = lora.events()
     if events & LoRa.RX_PACKET_EVENT:
@@ -123,10 +132,12 @@ def lora_cb(lora):
         print('Lora packet sent')
 
 lora.callback(trigger=(LoRa.RX_PACKET_EVENT | LoRa.TX_PACKET_EVENT), handler=lora_cb)
-"""
+
 time.sleep(10)
 _thread.start_new_thread(Send, ())
 
-_thread.start_new_thread(Recv, ())
+_thread.start_new_thread(Packet_buffer, ())
+
+#_thread.start_new_thread(Recv, ())
 
 _thread.start_new_thread(clear,())
